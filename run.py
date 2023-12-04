392e3756e0fc925f43d9d20134727ac8a10870a5
Sick4n <144610024+Sick4n@users.noreply.github.com>
2023-12-04 02:40:11 +0100

    Added board for holding ship locations  and board to display hits and misses

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+# Board for holding ship locations
+HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
+# Board for displaying hits and misses
+GUESS_BOARD = [[' '] * 8 for x in range(8)]
+
+letters_to_numbers {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7}
+
+def print_board(board):
+  print("  A B C D E F G H")
+  print(' =====================')
+  row_number = 1
+  for row in board:
+    print("%d|%s|" % (row_number, "|".join(row)))
+    row_number += 1
+  
+
+def create_ships():
+  pass  
+
+def get_ship_location():
+  pass
+
+def count_hit_ships():
+  pass
+
+create_ships()
+turns = 10 
+#while turns > 0
